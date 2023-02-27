e943c86cde60d719281d0dbd4de41f0be3a6ec9d
Chu Seng <yongchuseng@gmail.com>
2023-02-27 20:49:00 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require("express");
+
+//express app
+const app = express();
+
+
+//routes
+app.get('/', (req, res) => {
+    res.json({ mssg: 'Welcome to the app' })
+})
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('Now listening on port', process.env.PORT)
+})
+
